2c91a493b163bac249aab46f8ce5b2ffe78ef8e6
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-08-14 01:30:18 -0700

    add example for decoding and reencoding data

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! ## Decoding and Encoding example GTFS data example
+//! 
+//! ```rs
+//! use prost::Message;
+//! 
+//! let url = "https://lbtgtfs.lbtransit.com/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb";
+//! let response = reqwest::get(url).await.unwrap();
+//! let bytes = response.bytes().await.unwrap();
+//! let data: Result<gtfs_realtime::FeedMessage, prost::DecodeError> = prost::Message::decode(bytes.as_ref());
+//! let data = data.unwrap();
+//! 
+//! //encode back into protobuf
+//! let rencoded = data.encode_to_vec();
+//! ```
+
 #[macro_use]
 extern crate serde;
 
