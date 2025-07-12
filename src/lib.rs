3b3f8e122d4c3d61ceaefc0ad9027cc8c6427b55
Gavin Morrow <gavinfmorrow@gmail.com>
2025-07-11 21:38:07 -0400

    Re-export prost

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
 //! ## Decoding and Encoding example GTFS data example
-//! 
+//!
 //! ```rust
 //! use prost::Message;
-//! 
+//!
 //! let url = "https://lbtgtfs.lbtransit.com/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb";
 //! let response = reqwest::get(url).await.unwrap();
 //! let bytes = response.bytes().await.unwrap();
 //! let data: Result<gtfs_realtime::FeedMessage, prost::DecodeError> = prost::Message::decode(bytes.as_ref());
 //! let data = data.unwrap();
-//! 
+//!
 //! //encode back into protobuf
 //! let rencoded = data.encode_to_vec();
 //! ```
 
 #[macro_use]
 extern crate serde;
+
+pub use prost;
 
 // Include the `items` module, which is generated from items.proto.
 include!(concat!(env!("OUT_DIR"), "/transit_realtime.rs"));
